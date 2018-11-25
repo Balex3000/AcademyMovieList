1772240faad8a78d2281717fba7e160260323908
alexba <balex3000@hotmail.com>
2018-11-25 16:22:32 +0200

    Added usage of View Holder

---
+++ app/src/main/java/com/balex/android/academymovielist/MovieViewHolder.java
@@ -0,0 +1,10 @@
+package com.balex.android.academymovielist;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MovieViewHolder {
+
+    public ImageView image;
+    public TextView title, desc;
+}
--- app/src/main/java/com/balex/android/academymovielist/MoviesBaseAdapter.java
+++ app/src/main/java/com/balex/android/academymovielist/MoviesBaseAdapter.java
@@ -5,8 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -38,20 +36,59 @@ public class MoviesBaseAdapter extends BaseAdapter {
     @Override
     public View getView(int pPosition, View convertView, ViewGroup parent) {
         // Inflate our row and find our views
+
+        /* regular list view
+
         View rowItem = mInflater.inflate(R.layout.item_movie, parent, false);
 
         ImageView img = rowItem.findViewById(R.id.item_movie_iv);
         TextView title = rowItem.findViewById(R.id.tv_movie_title);
         TextView desc = rowItem.findViewById(R.id.tv_movie_desc);
+        */
+
+        // new approach
+        MovieViewHolder holder;
+
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.item_movie, parent, false);
+            // approach 2.2 with view holder
+            holder = new MovieViewHolder();
+            holder.image = convertView.findViewById(R.id.item_movie_iv);
+            holder.title = convertView.findViewById(R.id.tv_movie_title);
+            holder.desc = convertView.findViewById(R.id.tv_movie_desc);
+            convertView.setTag(holder);
+        } else {
+            // part of aproach 2.2
+            holder = (MovieViewHolder) convertView.getTag();
+        }
+
+        // approach 2.1
+        /*
+        ImageView img = convertView.findViewById(R.id.item_movie_iv);
+        TextView title = convertView.findViewById(R.id.tv_movie_title);
+        TextView desc = convertView.findViewById(R.id.tv_movie_desc);
+        */
 
         // Get data for this specific row
         MovieModel movie = getItem(pPosition);
 
         // Fill our view with our data
+        // approach v1 and 2.1
+        /*
         img.setImageResource(movie.getImageResourceId());
         title.setText(movie.getTitle());
         desc.setText(movie.getDesc());
+        */
 
-        return rowItem;
+        // approach 2.2 with view holder
+        holder.image.setImageResource(movie.getImageResourceId());
+        holder.title.setText(movie.getTitle());
+        holder.desc.setText(movie.getDesc());
+
+        // regular list view approach
+        //return rowItem;
+
+        // new approach 2.1 and 2.2
+        return convertView;
     }
 }
