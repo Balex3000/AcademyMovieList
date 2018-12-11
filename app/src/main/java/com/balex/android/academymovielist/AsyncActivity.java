1ae2f4cbe74845e60b15fbe0ce1913baf041e729
alexba <balex3000@hotmail.com>
2018-12-11 17:05:08 +0200

    tried to add logic to deal with change in orientation but it doesn't work properly yet

--- app/src/main/java/com/balex/android/academymovielist/AsyncActivity.java
+++ app/src/main/java/com/balex/android/academymovielist/AsyncActivity.java
@@ -1,17 +1,23 @@
 package com.balex.android.academymovielist;
 
+import android.content.res.Resources;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class AsyncActivity extends AppCompatActivity {
 
+    private static final String SAVED_STATE_CURRENT_NUMBER = "LAST_NUMBER";
+    private static final Integer COUNT_DOWN_STARTS_FROM = 5;
+
     private TextView countDownTextView;
     private CountDown task;
+    private static Integer currentNumber;
+    private static boolean isRunning;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,29 +25,64 @@ public class AsyncActivity extends AppCompatActivity {
         setContentView(R.layout.activity_async);
 
         countDownTextView = findViewById(R.id.tv_async);
+
+        if (savedInstanceState != null) {
+            currentNumber = savedInstanceState.getInt(SAVED_STATE_CURRENT_NUMBER);
+            task = new CountDown(countDownTextView);
+            isRunning = true;
+            task.execute(currentNumber);
+        }
     }
 
     public void startButtonActions (View view) {
         //Toast.makeText (view.getContext(), "start async", Toast.LENGTH_SHORT).show();
         task = new CountDown(countDownTextView);
-        task.execute(3,2,1);
+        //task.execute(3,2,1);
+        isRunning = true;
+        task.execute(COUNT_DOWN_STARTS_FROM);
     }
 
     public void cancelButtonActions (View view) {
-        task.cancel(true);
+        if (task != null) {
+            task.cancel(true);
+            isRunning = false;
+        }
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (task != null) { task.cancel(true); }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (task != null) { task.cancel(true); }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        if (isRunning) {
+            outState.putInt(SAVED_STATE_CURRENT_NUMBER, currentNumber);
+        }
+    }
+
+    /** Async Task */
     public static class CountDown extends AsyncTask <Integer, Integer, String> {
 
         private TextView asyncTextView;
 
-        public CountDown (TextView pTextView) {
+        CountDown (TextView pTextView) {
             asyncTextView = pTextView;
         }
 
-        protected String doInBackground(Integer ... numbers) {
-            //Toast.makeText(asyncTextView.getContext(), "doInBackground my", Toast.LENGTH_SHORT ).show();
-            for (Integer number: numbers) {
+        //protected String doInBackground(Integer ... numbers) {
+        protected String doInBackground(Integer ... startNumber) {
+
+            //for (Integer number: numbers) {
+            for (Integer number = startNumber[0]; number > 0; number--) {
                 if (!isCancelled()) {
                     publishProgress(number);
                     try {
@@ -54,6 +95,7 @@ public class AsyncActivity extends AppCompatActivity {
                 }
             }
 
+            isRunning = false;
             return "Done!";
         }
 
@@ -61,8 +103,15 @@ public class AsyncActivity extends AppCompatActivity {
             asyncTextView.setText("");
         }
 
+        /**
         protected void onProgressUpdate(Integer ... number) {
             asyncTextView.setText(String.valueOf(number[0]));
+        }
+         */
+
+        protected void onProgressUpdate(Integer ... number) {
+            asyncTextView.setText(String.valueOf(number[0]));
+            currentNumber = number[0];
         }
 
         protected void onPostExecute(String result) {
@@ -71,7 +120,7 @@ public class AsyncActivity extends AppCompatActivity {
 
         @Override
         protected void onCancelled() {
-            asyncTextView.setText("Cancelled!");
+            asyncTextView.setText("cancelled");
         }
     }
 
