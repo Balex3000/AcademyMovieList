209c0df73d10b3535e1ba82ae99bd5e64f15952a
alexba <balex3000@hotmail.com>
2018-12-17 17:52:17 +0200

    added all the logic from the exercise #6 but there are few loopholes that I don't know how to do. So it is probably doesn't work

--- app/src/main/java/com/balex/android/academymovielist/BGServiceActivity.java
+++ app/src/main/java/com/balex/android/academymovielist/BGServiceActivity.java
@@ -1,7 +1,11 @@
 package com.balex.android.academymovielist;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,7 +13,12 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import static com.balex.android.academymovielist.BGServiceActivity.BackgroundProgressReceiver.PROGRESS_UPDATE_ACTION;
+
 public class BGServiceActivity extends AppCompatActivity {
+
+    private BackgroundProgressReceiver mBackgroundProgressReceiver;
+    public boolean isDestroyed;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,5 +46,40 @@ public class BGServiceActivity extends AppCompatActivity {
                 startService(intent);
             }
         });
+    }
+
+    protected void onHandleIntent (Intent intent) {
+        isDestroyed = false;
+
+        for (int i=0; i <= 100; i++) {
+            SystemClock.sleep(100);
+            Intent broadcastIntent = new Intent(BackgroundProgressReceiver.PROGRESS_UPDATE_ACTION);
+            broadcastIntent.putExtra(BackgroundProgressReceiver.PROGRESS_VALUE_KEY, i);
+            sendBroadcast(broadcastIntent);
+        }
+
+    }
+
+    private void subscribeForProgressUpdate() {
+        if (mBackgroundProgressReceiver == null) {
+            mBackgroundProgressReceiver = new BackgroundProgressReceiver();
+        }
+
+        IntentFilter progressUpdateActionFilter = new IntentFilter(PROGRESS_UPDATE_ACTION);
+        registerReceiver(mBackgroundProgressReceiver, progressUpdateActionFilter);
+    }
+
+    public class BackgroundProgressReceiver extends BroadcastReceiver {
+
+        public static final String PROGRESS_UPDATE_ACTION = "PROGRESS_UPDATE_ACTION";
+        public static final String PROGRESS_VALUE_KEY = "PROGRESS_VALUE_KEY";
+        public static final String SERVICE_STATUS = "SERVICE_STATUS";
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int progress = intent.getIntExtra(PROGRESS_VALUE_KEY, -1);
+
+            //TODO: parse progress value and update the mProgressValue TextView with relevant value
+        }
     }
 }
--- app/src/main/java/com/balex/android/academymovielist/Service1.java
+++ app/src/main/java/com/balex/android/academymovielist/Service1.java
@@ -2,11 +2,22 @@ package com.balex.android.academymovielist;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.widget.Toast;
+import android.os.Process;
 
 public class Service1 extends Service {
+
+    private final static String TAG = "TAG";
+    private Looper mServiceLooper;
+    private ServiceHandler mServiceHandler;
+
 
     @Nullable
     @Override
@@ -15,12 +26,58 @@ public class Service1 extends Service {
     }
 
     @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // To avoid CPU blocking we create a background handler to run our service
+        // TODO verify what is the TAG
+        HandlerThread thread = new HandlerThread(TAG, Process.THREAD_PRIORITY_BACKGROUND);
+
+        // start the new Handler Thread
+        thread.start();
+
+        mServiceLooper = thread.getLooper();
+
+        // start the service using the background handler
+        mServiceHandler = new ServiceHandler(mServiceLooper);
+    }
+
+    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         //return super.onStartCommand(intent, flags, startId);
 
         Toast.makeText(getApplicationContext(), "Service1 onStartCommand", Toast.LENGTH_SHORT).show();
 
+        // TODO isDestroyed = false;
+
+        // call a new service handler, The service ID can be used to identify the service
+        Message message = mServiceHandler.obtainMessage();
+
+        message.arg1 = startId;
+        mServiceHandler.sendMessage(message);
+
         return START_STICKY;
 
+    }
+
+    private final class ServiceHandler extends Handler {
+
+        public ServiceHandler(Looper looper) {super(looper);}
+
+        @Override
+        public void handleMessage(Message msg) {
+
+            // TODO add && !isDestroyed
+            for (int i=0; i <= 100; i++) {
+                SystemClock.sleep(100);
+                Intent broadcastIntent = new Intent(BGServiceActivity.BackgroundProgressReceiver.PROGRESS_UPDATE_ACTION);
+                broadcastIntent.putExtra(BGServiceActivity.BackgroundProgressReceiver.PROGRESS_VALUE_KEY, i);
+                sendBroadcast(broadcastIntent);
+            }
+
+            // the msg.arg1 is the startId used in the onStartCommand so we can track the running service here
+            stopSelf(msg.arg1);
+
+        }
     }
 }
